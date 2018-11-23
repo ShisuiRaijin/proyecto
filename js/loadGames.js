8ecb52c92a979e367eec58d4c235656809b914ef
unknown <imauror98@gmail.com>
2018-11-22 23:13:26 -0300

    json modification

--- js/loadGames.js
+++ js/loadGames.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContenLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
 
 	/* Request for the json */
@@ -11,6 +11,15 @@ document.addEventListener("DOMContenLoaded", function(){
 	r.onload = function loadGames() {
 		var json = r.response;
 
-		
+		console.log(json)
+		var game = json["csgo"]
+		console.log(game)
+
+		var elem = document.getElementById("gameTitle");
+		elem.innerText = game.title;
+	
+	
+		elem = document.getElementById("gameTrailer");
+		elem.innerHTML = elem.innerHTML + game["trailer"]
 	}
 })
